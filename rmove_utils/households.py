9b09c730c81f6da894012218e43ee91aabef8e42
Drew Cooper <drew.cooper@sfcta.org>
2019-09-04 16:58:55 -0700

    Add placeholder function add_home_location
    Signed-off-by: Drew Cooper <drew.cooper@sfcta.org>

--- rmove_utils/households.py
+++ rmove_utils/households.py
@@ -5,8 +5,10 @@ date: April 16, 2019
 
 import os
 import pandas as pd
+import geopandas as gpd
 
 from .base import Base
+from .utils import point_in_polygon
 
 class Households(Base):
     expected_columns = ['hhid',
@@ -50,5 +52,16 @@ class Households(Base):
     
     def __init__(self, file_name=None, sep='\t', error_level=0):
         super().__init__(file_name, sep, error_level)
+        self.home_x = None
+        self.home_y = None
         
-                    
+    def add_home_location(self, poly, field, name='home_location'):
+        '''
+        TODO: Testing needed.
+        poly: a GeoDataFrame of polygons
+        field: the field name in poly to assign to home_location
+        '''
+        home = gpd.GeoDataFrame(index=self.data.index, 
+                                geometry=self.data.apply(lambda x: Point(x[self.home_x],x[self.home_y]),
+                                                         axis=1))
+        self.data[name] = point_in_polygon(home, poly, field)
