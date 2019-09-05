c5f86a1e82f015094958f76272eb0d0d385614e1
Drew Cooper <drew.cooper@sfcta.org>
2019-09-04 17:03:16 -0700

    Race/ethinicity classification utils
    Signed-off-by: Drew Cooper <drew.cooper@sfcta.org>

--- rmove_utils/utils.py
+++ rmove_utils/utils.py
@@ -8,8 +8,29 @@ def point_in_polygon(points, polygons, name_field):
     return s[name_field]
     
 def ethnicity(row, descriptions, values):
-    if row['ethnicity_no_answer'] == 1:
+    '''
+    Impute an race/ethnicity based on responses for each race/ethnicity field
+    '''
+    error_values = [-9998, 995, 999]
+    eth = ''
+    err = ''
+    
+    # first check if they're all an error
+    # if any of them have an error code, then all of them do
+    for ev in error_values:
+        if row[['ethnicity_af_am','ethnicity_aiak','ethnicity_asian',
+                'ethnicity_hapi','ethnicity_hisp','ethnicity_white',
+                'ethnicity_other','ethnicity_multi']].eq(ev).all():
+            return values['ethnicity_af_am'][ev]
+                    
+    # If no answer, return no answer
+    if (row['ethnicity_no_answer'] == 1 or 
+        row[['ethnicity_af_am','ethnicity_aiak','ethnicity_asian',
+             'ethnicity_hapi','ethnicity_hisp','ethnicity_white',
+             'ethnicity_other','ethnicity_multi']].eq(0).all()):
         return 'No Answer'
+    
+    # Now impute a racial/ethnic category from valid responses
     if (row['ethnicity_white'] == 1 and not
         row[['ethnicity_af_am','ethnicity_aiak',
              'ethnicity_asian','ethnicity_hapi',
@@ -22,14 +43,24 @@ def ethnicity(row, descriptions, values):
              'ethnicity_other','ethnicity_multi']].eq(1).any()):
         return 'White Hispanic'
     if row['ethnicity_multi'] == 1 :
-        return 'Multi'
-    if row[['ethnicity_af_am','ethnicity_aiak','ethnicity_asian','ethnicity_hapi','ethnicity_hisp','ethnicity_white','ethnicity_other']].sum() == 1:
-        for x in ['ethnicity_af_am','ethnicity_aiak','ethnicity_asian','ethnicity_hapi','ethnicity_hisp','ethnicity_white','ethnicity_other']:
+        return 'Multiple'
+    if row[['ethnicity_af_am','ethnicity_aiak','ethnicity_asian',
+            'ethnicity_hapi','ethnicity_hisp','ethnicity_white',
+            'ethnicity_other']].sum() == 1:
+        for x in ['ethnicity_af_am','ethnicity_aiak','ethnicity_asian',
+                  'ethnicity_hapi','ethnicity_hisp','ethnicity_white',
+                  'ethnicity_other']:
             if row[x] == 1:
                 return descriptions[x].replace('Ethnicity: ', '')
-    if row[['ethnicity_af_am','ethnicity_aiak','ethnicity_asian','ethnicity_hapi','ethnicity_hisp','ethnicity_white','ethnicity_other']].eq(1).any():
-        return 'Multi'
-    return 'exception'
+    if row[['ethnicity_af_am','ethnicity_aiak','ethnicity_asian',
+            'ethnicity_hapi','ethnicity_hisp','ethnicity_white',
+            'ethnicity_other']].sum() > 1:
+        return 'Multiple'
+    
+    raise Exception('Could not impute race/ethnicity for {}'.format(row[['ethnicity_af_am','ethnicity_aiak',
+                         'ethnicity_asian','ethnicity_hapi',
+                         'ethnicity_hisp','ethnicity_other',
+                         'ethnicity_multi']]))
     
 def ethnicity_simple(row, descriptions, values):
     if row['ethnicity_no_answer'] == 1:
